ffbe48d58544a2b21123aa81555f4f1d41298399
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-17 20:53:02 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,17 @@
 
 def key_for_min_value(name_hash)
 
+    lowest_value = nil
+    lowest_key = nil
+      
+      name_hash.each do |key, value|
+        lowest_key = key if lowest_key.nil?
+        lowest_value = value if lowest_value.nil?
+        if value < lowest_value
+          lowest_value = value
+          lowest_key = key
+        end
+      end
+      
+  lowest_key
 end
